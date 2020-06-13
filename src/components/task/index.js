dd7bf48ba274dc36360491c11dadd0916e6268e5
Meisam Malekzadeh <meisammalekzadeh@gmail.com>
2020-06-13 15:40:06 +0430

    showing selected task in diffrent color

--- src/components/task/index.js
+++ src/components/task/index.js
@@ -1,10 +1,14 @@
 import React from "react";
-import { Typography,  Card,  CardContent,  CardActionArea} from "@material-ui/core";
+import {  Typography,  Card,  CardContent,  CardActionArea,} from "@material-ui/core";
 import classes from "./style.module.css";
 
-const Task = ({task}) => {
+const Task = ({ task, selected , onSelected }) => {
   return (
-    <Card className={classes.task}>
+    <Card
+      className={classes.task}
+      style={selected ? { backgroundColor: '#fff176' } : null}
+      onClick = {() => onSelected(task.id)}
+    >
       <CardActionArea>
         <CardContent>
           <Typography gutterBottom variant="h5" component="h2">
