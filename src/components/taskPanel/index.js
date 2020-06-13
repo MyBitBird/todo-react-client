8ddb6b551c58a722970285cb0d39c6df58c44b15
Meisam Malekzadeh <meisammalekzadeh@gmail.com>
2020-06-13 16:45:38 +0430

    moving card added

--- src/components/taskPanel/index.js
+++ src/components/taskPanel/index.js
@@ -5,9 +5,8 @@ import { useSelector } from "react-redux";
 import Task from "../task";
 import ButtonsPanel from "../buttonsPanel";
 import Dialog from "../dialog";
-import {addTask}  from '../../store/actions/tasks'
+import { addTask, updateTaskType } from "../../store/actions/tasks";
 import { useDispatch } from "react-redux";
-
 
 const TaskPanel = ({ panel }) => {
   const tasks = useSelector((state) =>
@@ -17,19 +16,24 @@ const TaskPanel = ({ panel }) => {
   const dispatch = useDispatch();
 
   const [isDialogOpen, setDialogOpen] = useState(false);
+  const [title, setTitle] = useState("");
+  const [desc, setDesc] = useState("");
+  const [selectedTaskId, setSelectedTaskId] = useState(null);
+
   const handleOpenDialog = () => setDialogOpen(true);
-  const [title , setTitle] = useState('');
-  const [desc , setDesc] = useState('');
-  const [selectedTaskId , setSelectedTaskId] = useState(null)
-
-  const handleTaskClick = id =>{
-    setSelectedTaskId( selectedTaskId === id ? null : id)
-  }
-
+  
+  const handleTaskClick = (id) => {
+    setSelectedTaskId(selectedTaskId === id ? null : id);
+  };
 
   const handleAddTask = async () => {
-    dispatch(await addTask({title: title , desc: desc , type: panel.type}));
+    dispatch(await addTask({ title: title, desc: desc, type: panel.type }));
     setDialogOpen(false);
+  };
+
+  const handleMoveTask = async () => {
+    dispatch(await updateTaskType(tasks.filter((x) => x.id === selectedTaskId)[0]));
+    setSelectedTaskId(null);
   };
 
   return (
@@ -41,44 +45,53 @@ const TaskPanel = ({ panel }) => {
       >
         <Typography variant="h6" component="h6" className={classes.header}>
           {`#${panel.title}`}
-          <ButtonsPanel onOpenDialog={handleOpenDialog} selectedId={selectedTaskId} />
+          <ButtonsPanel
+            onOpenDialog={handleOpenDialog}
+            selectedId={selectedTaskId}
+            onMoveTask={handleMoveTask}
+          />
         </Typography>
         {tasks.map((task, key) => (
-          <Task task={task} key={key} selected={selectedTaskId === task.id} onSelected={handleTaskClick} />
+          <Task
+            task={task}
+            key={key}
+            selected={selectedTaskId === task.id}
+            onSelected={handleTaskClick}
+          />
         ))}
       </Paper>
 
       <Dialog open={isDialogOpen}>
-          <DialogContent>
-            <FormControl>
-              <TextField
-                autoFocus
-                margin="dense"
-                id="title"
-                label="Task Title"
-                fullWidth
-                onChange={e => setTitle(e.target.value)}
-              />
-              <TextField
-                autoFocus
-                margin="dense"
-                id="desc"
-                label="Task Description"
-                rows={4}
-                fullWidth
-                multiline
-                onChange={e => setDesc(e.target.value)}
-              />
-            </FormControl>
-          </DialogContent>
-          <DialogActions>
-            <Button onClick={() => setDialogOpen(false)} color="primary">
-              Cancel
-            </Button>
-            <Button color="primary" onClick={()=>handleAddTask()}>
-              Save
-            </Button>
-          </DialogActions>
+        <DialogContent>
+          <FormControl>
+            <TextField
+              autoFocus
+              margin="dense"
+              id="title"
+              label="Task Title"
+              fullWidth
+              onChange={(e) => setTitle(e.target.value)}
+            />
+            <TextField
+              autoFocus
+              margin="dense"
+              id="desc"
+              label="Task Description"
+              rows={4}
+              fullWidth
+              multiline
+              onChange={(e) => setDesc(e.target.value)}
+            />
+          </FormControl>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={() => setDialogOpen(false)} color="primary">
+            Cancel
+          </Button>
+          <Button color="primary" onClick={() => handleAddTask()}>
+            Save
+          </Button>
+        </DialogActions>
       </Dialog>
     </>
   );
