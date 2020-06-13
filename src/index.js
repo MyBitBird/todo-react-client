381f2690af8914ea3d6ae9f69e6a540a752c629d
Meisam Malekzadeh <meisammalekzadeh@gmail.com>
2020-06-13 11:43:19 +0430

    add store in project

--- src/index.js
+++ src/index.js
@@ -1,14 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { createStore, applyMiddleware, compose, combineReducers } from "redux";
+import tasksReducer from "./store/reducers/tasks";
+import { Provider } from "react-redux";
+import thunk from 'redux-thunk';
+
+
+const reducers = combineReducers({
+  tasks: tasksReducer,
+});
+
+const store = createStore(reducers , applyMiddleware(thunk));
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <ToastContainer />
+      <App />
+    </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
