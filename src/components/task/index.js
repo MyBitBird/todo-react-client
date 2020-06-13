5ad8cec344316958f0da93dcb833d99890f6d2a8
Meisam Malekzadeh <meisammalekzadeh@gmail.com>
2020-06-13 11:44:21 +0430

    load primary data from redux

--- src/components/task/index.js
+++ src/components/task/index.js
@@ -1,13 +1,32 @@
 import React from "react";
-import { Paper,Typography } from "@material-ui/core";
-import classes from './style.module.css'
+import {  Paper,  Typography,  Card,  CardContent,CardActionArea} from "@material-ui/core";
+import classes from "./style.module.css";
+import { useSelector } from 'react-redux'
 
-const Task = ({task}) => {
+
+const Task = ({ task }) => {
+
+  const tasks = useSelector(state => state.tasks.tasks.filter( x => x.type === task.type));
+  
   return (
-    <Paper elevation={3} className={classes.task}>
-      <Typography variant="h6" component="h6"  className={classes.taskHeader}  >
+    <Paper elevation={3} className={classes.container}>
+      <Typography variant="h6" component="h6" className={classes.header}>
         {`#${task.title}`}
       </Typography>
+      {tasks.map((task, key) => (
+        <Card key={key} className={classes.task}>
+          <CardActionArea>
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="h2">
+                {task.title}
+              </Typography>
+              <Typography variant="body2" color="textSecondary" component="p">
+                {task.desc}
+              </Typography>
+            </CardContent>
+          </CardActionArea>
+        </Card>
+      ))}
     </Paper>
   );
 };
--- src/pages/tasks/index.js
+++ src/pages/tasks/index.js
@@ -1,17 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Grid } from "@material-ui/core";
 import Task from "../../components/task";
-import classes from './style.module.css'
+import classes from "./style.module.css";
+import { useDispatch } from "react-redux";
+import { getTasks } from "../../store/actions/tasks";
 
-const tasks = [
+const taskPanels = [
   { title: "ToDo", type: 1 },
   { title: "Doing", type: 2 },
   { title: "Done", type: 3 },
 ];
 const Tasks = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      dispatch(await getTasks());
+    };
+    fetchData();
+  }, []);
+
   return (
     <Grid container spacing={2} className={classes.container}>
-      {tasks.map((task, key) => (
+      {taskPanels.map((task, key) => (
         <Grid key={key} item md={4} xs={12}>
           <Task task={task} />
         </Grid>
