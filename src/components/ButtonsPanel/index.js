27e98e3a8080076471b8f5f26fe6f2ddd535e862
Meisam Malekzadeh <meisammalekzadeh@gmail.com>
2020-06-13 16:44:47 +0430

    add movetask handler

--- src/components/ButtonsPanel/index.js
+++ src/components/ButtonsPanel/index.js
@@ -3,40 +3,37 @@ import { IconButton } from "@material-ui/core";
 import classes from "./style.module.css";
 import DeleteIcon from "@material-ui/icons/Delete";
 import AddIcon from "@material-ui/icons/Add";
-import EditIcon from "@material-ui/icons/Edit";
+import ArrowIcon from "@material-ui/icons/ArrowForward";
 
 import Theme from "../theme";
-const ButtonsPanel = ({ selectedId, onOpenDialog }) => {
+const ButtonsPanel = (props) => {
   return (
     <div className={classes.container}>
-      {!selectedId ? (
+      {!props.selectedId ? 
         <IconButton
-          aria-label="add"
           style={Theme.palette.white}
           size="small"
-          onClick={onOpenDialog}
-        >
+          onClick={props.onOpenDialog}>
           <AddIcon fontSize="inherit" />
         </IconButton>
-      ) : (
+       : 
         <>
           <IconButton
-            aria-label="edit"
-            style={Theme.palette.white}
-            size="small"
-          >
-            <EditIcon fontSize="inherit" />
-          </IconButton>
-          &nbsp;
-          <IconButton
-            aria-label="delete"
             style={Theme.palette.white}
             size="small"
           >
             <DeleteIcon fontSize="inherit" />
           </IconButton>
+          &nbsp;
+          <IconButton
+            style={Theme.palette.white}
+            size="small"
+            onClick = {props.onMoveTask}
+          >
+            <ArrowIcon fontSize="inherit" />
+          </IconButton>
         </>
-      )}
+      }
     </div>
   );
 };
