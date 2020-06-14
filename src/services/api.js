9c79fd7e79a7478a4f59474a3609ac644c274bb5
Meisam Malekzadeh <meisammalekzadeh@gmail.com>
2020-06-14 11:13:29 +0430

    apiloading component added

--- src/services/api.js
+++ src/services/api.js
@@ -1,43 +1,56 @@
 import axios from "axios";
 import { toast } from "react-toastify";
+import { useDispatch } from "react-redux";
+import { setLoading } from "../store/actions/global";
+import Loading from "./../components/loading/index";
+import React from "react";
 
-axios.defaults.baseURL = "http://localhost:8080/api/";
+export const ApiLoading = () => {
+  axios.defaults.baseURL = "http://localhost:8080/api/";
 
-axios.interceptors.request.use((config) => {
-  return config;
-});
-axios.interceptors.response.use(
-  (response) => {
-    toast.dismiss();
-    return response;
-  },
-  (error) => {
-    toast.dismiss();
+  const dispatch = useDispatch();
+  axios.interceptors.request.use((config) => {
+    dispatch(setLoading(true));
+    return config;
+  });
 
-    const expectedError =
-      error.response &&
-      error.response.status >= 400 &&
-      error.response.status <= 500;
+  axios.interceptors.response.use(
+    (response) => {
+      toast.dismiss();
+      dispatch(setLoading(false));
+      return response;
+    },
+    (error) => {
+      dispatch(setLoading(false));
+      toast.dismiss();
 
-    console.log("error", error.response);
+      const expectedError =
+        error.response &&
+        error.response.status >= 400 &&
+        error.response.status <= 500;
 
-    if (expectedError) {
-      if (error.response.status === 400) toast.error(`Invalid Data ${error.response.data}`);
-      if (error.response.status === 404) toast.error(`Not Found`);
-      else if (error.response.status === 500) toast.error("Internal Server");
-    } else {
-      if ((error + "").includes("Network")) toast.error("Network Error");
-      else toast.error("UnExpected Error");
+      if (expectedError) {
+        if (error.response.status === 400)
+          toast.error(`Invalid Data ${error.response.data}`);
+        if (error.response.status === 404) toast.error(`Not Found`);
+        else if (error.response.status === 500) toast.error("Internal Server");
+      } else {
+        if ((error + "").includes("Network")) toast.error("Network Error");
+        else toast.error("UnExpected Error");
+      }
+
+      return Promise.reject(error);
     }
+  );
 
-    return Promise.reject(error);
-  }
-);
+  return <Loading />;
+};
 
 export default {
   get: axios.get,
   post: axios.post,
   put: axios.put,
   delete: axios.delete,
-  patch: axios.patch
+  patch: axios.patch,
+  ApiLoading: ApiLoading,
 };
